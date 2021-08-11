d3211af480280413f99b1178c54b1f4ead1d9be4
Audrey Bourret <audrey.bourret@usherbrooke.ca>
2021-08-11 09:19:37 -0400

    Add 2 structure functions

---
+++ R/structure.R
@@ -0,0 +1,53 @@
+#' @title Read structure q files
+#'
+#' @description
+#' str_readq(.) read structure q file as produce by faststructure
+#'
+#' @details
+#' more details to come ...
+#'
+#' @param file Path to the file
+#'
+#' @examples
+#' # provide some examples of how to use your function
+#'  file.path <- ".results_job_k3_r1_q" 
+#'  str_readq(file.path)
+#'
+#' @export
+str_readq <- function(file){
+  struct.int <-  readr::read_delim(file,
+                           delim = " ", trim_ws = T,
+                           col_names = F) %>% 
+    dplyr::select(-c(paste0("X", ncol(.))))
+  names(struct.int) <- c("ID_GQ", "PopNum", paste0("Q", 1:(base::ncol(struct.int)-2)))
+  
+  res <- struct.int %>% dplyr::mutate(K = ncol(.)-2 ) %>% 
+    tidyr::pivot_longer(stringr::str_subset(names(.), "ID_GQ|PopNum|K", negate = T), 
+                 names_to = "Qpop", values_to = "Qvalue") 
+  
+  return(res)
+  
+}
+
+#' @title Read structure summary file
+#'
+#' @description
+#' str_readsummary(.) read structure  summary file as produced by faststructure
+#'
+#' @details
+#' more details to come ...
+#'
+#' @param file Path to the file
+#'
+#' @examples
+#' # provide some examples of how to use your function
+#'  file.path <- ".results_job_k3_r1_q" 
+#'  str_readsummary(file.path)
+#'
+#' @export
+
+str_readsummary <- function(file) {
+  readr::read_csv(file,
+         skip = 1) %>% select(-c(`1`))
+}
+
