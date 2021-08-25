5e328366c1905b170efd7d7ec167a917adb0cb9f
biodray <audrey.bourret@usherbrooke.ca>
2021-08-25 11:31:51 -0400

    Add man on data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' @title Structure run summary
+#'
+#' @description test
+#'
+#' \itemize{
+#'   \item run_id. to be define
+#'   \item burnin. 
+#' }
+#'
+#' @docType data
+#' @name strcSummary
+#' @usage data(strcSummary)
+#' @format A tibble with 30 rows and 8 variables
+NULL
